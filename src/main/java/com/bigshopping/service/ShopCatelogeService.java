1bd62b98dfd077df5af2bdb09b18a06b15303acd
lirui167734886 <42735449+lirui167734886@users.noreply.github.com>
2019-11-13 11:26:14 +0800

    框架
     Conflicts:
    	.idea/encodings.xml
    	pom.xml
    
    Signed-off-by: lirui <1677348863@qq.com>

---
+++ src/main/java/com/bigshopping/service/ShopCatelogeService.java
@@ -0,0 +1,16 @@
+package com.bigshopping.service;
+
+import com.baomidou.mybatisplus.service.IService;
+import com.bigshopping.bean.ShopCateloge;
+
+/**
+ * <p>
+ *  服务类
+ * </p>
+ *
+ * @author LiRui
+ * @since 2019-11-12
+ */
+public interface ShopCatelogeService extends IService<ShopCateloge> {
+
+}
---
+++ src/main/java/com/bigshopping/service/impl/ShopCatelogeServiceImpl.java
@@ -0,0 +1,20 @@
+package com.bigshopping.service.impl;
+
+import com.baomidou.mybatisplus.service.impl.ServiceImpl;
+import com.bigshopping.bean.ShopCateloge;
+import com.bigshopping.mapper.ShopCatelogeMapper;
+import com.bigshopping.service.ShopCatelogeService;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ *  服务实现类
+ * </p>
+ *
+ * @author LiRui
+ * @since 2019-11-12
+ */
+@Service
+public class ShopCatelogeServiceImpl extends ServiceImpl<ShopCatelogeMapper, ShopCateloge> implements ShopCatelogeService {
+
+}
